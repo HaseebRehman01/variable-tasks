586c0d99a454af3a729aa4ad98b8960f6cfbf21c
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-14 20:39:36 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// task 1
+// var name;
+
+
+// task 2
+// var name = "Haseeb ur rehman";
+
+
+
+// task 3
+// var message ="Hello world!"
+// alert(message)
+
+
+// task 4
+// var name = "john doe";
+// var age ="15 year old"
+// var qualification = "Certified mobile application development"
+// alert(name)
+// alert(age)
+// alert(qualification)
+
+// /task 5
+// var pizza = "pizza\npizz\npiz\npi\np";
+// alert(pizza)
+
+
+// task 6
+// var email = "haseeburrehman763@gmail.com";
+// alert("My email address is "
+// +email)
+
+
+// task 7
+// var book =  "way to learn JavaScript";
+// alert("I am trying to learn from the book A smarter\n"+book)
+
+
+// task 8
+// var content = "Yeah! I can write html content through javascript"
+// document.write(content)
+
+
+// task 9
+// var content ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(content)
+
+
+
+
+// complete
